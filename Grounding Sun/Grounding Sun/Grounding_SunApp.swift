cf29004bed519cb19a0f1b02783eaec1a4fd4c07
dianaescu <dianateodorescu@veed.io>
2026-01-12 21:14:12 +0000

    Initial SwiftUI project

---
+++ Grounding Sun/Grounding Sun/Grounding_SunApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grounding_SunApp.swift
+//  Grounding Sun
+//
+//  Created by Diana on 12/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Grounding_SunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
